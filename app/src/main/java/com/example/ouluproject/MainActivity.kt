145536f0305bfa6b7c0774464f89304647cfd2cd
SaoirseReposar <C22448686@mytudublin.ie>
2025-03-16 20:15:53 +0200

    test commit 2

--- app/src/main/java/com/example/ouluproject/MainActivity.kt
+++ app/src/main/java/com/example/ouluproject/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+//ss
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
