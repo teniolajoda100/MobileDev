e1fa18abd4434e25bd75a984b0a3907e14eaefa5
annnnnne0811 <annnnnneflores2000@yahoo.com>
2025-03-18 02:47:50 +0200

    testing?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.ouluproject"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.ouluproject"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
